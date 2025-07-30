94503f8c493d35c8ecfce6859288240ba1426adb
Caio-Hernandes <caiohernandesvsilva00@gmail.com>
2025-07-30 16:32:58 -0300

    mudanças nas validações dos formulários

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,4 +2,16 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+
+async function testConnection() {
+  try {
+    await prisma.$connect();
+    console.log('✅ Conectado ao banco de dados com sucesso!');
+  } catch (err) {
+    console.error('❌ Erro ao conectar ao banco de dados:', err);
+  }
+}
+
+testConnection();
+
 module.exports = prisma;
