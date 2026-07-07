5e16f90d8f199686fd4f66272a47e5ca90831a39
Caio Hernandes <caiohernandesvsilva00@gmail.com>
2026-07-07 09:53:16 -0300

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -6,9 +6,9 @@ const prisma = new PrismaClient();
 async function testConnection() {
   try {
     await prisma.$connect();
-    console.log('✅ Conectado ao banco de dados com sucesso!');
+    console.log('Conectado ao banco de dados com sucesso!');
   } catch (err) {
-    console.error('❌ Erro ao conectar ao banco de dados:', err);
+    console.error('Erro ao conectar ao banco de dados:', err);
   }
 }
 
